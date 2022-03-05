030f3c788c35c32a1315e8a92297266dbd23afa5
Byby <dvl.java@gmail.com>
2022-03-05 23:06:39 +0300

    format

--- src/main/java/org/byby/App.java
+++ src/main/java/org/byby/App.java
@@ -1,12 +1,10 @@
 package org.byby;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 
@@ -19,12 +17,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        var javaVersion = SystemInfo.javaVersion();
-//        var javafxVersion = SystemInfo.javafxVersion();
-//
-//        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        //var scene = new Scene(new StackPane(label), 640, 480);
-
         scene = new Scene(loadFXML("main"));
 
         stage.setScene(scene);
@@ -32,9 +24,10 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader loader = new  FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return loader.load();
     }
+
     public static void main(String[] args) {
         launch();
     }
